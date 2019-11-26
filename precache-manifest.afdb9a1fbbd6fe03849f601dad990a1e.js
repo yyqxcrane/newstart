565ac362a2c8ab7d45e7a960a64e07ccdfe88e3f
zhangsong <zhangsong@beisen.com>
2019-11-26 11:08:28 +0800

    Updates

---
+++ precache-manifest.afdb9a1fbbd6fe03849f601dad990a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c20ee18c408b98febea425c87971f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42ae1cedaafdb5b04812",
+    "url": "./static/css/2.78b20d3f.chunk.css"
+  },
+  {
+    "revision": "d0c023bdd940dbafd55b",
+    "url": "./static/css/main.dbb8c967.chunk.css"
+  },
+  {
+    "revision": "42ae1cedaafdb5b04812",
+    "url": "./static/js/2.69bef28e.chunk.js"
+  },
+  {
+    "revision": "d0c023bdd940dbafd55b",
+    "url": "./static/js/main.89bf8dea.chunk.js"
+  },
+  {
+    "revision": "68b6a74012b688aeccfc",
+    "url": "./static/js/runtime-main.64988bfb.js"
+  }
+]);
